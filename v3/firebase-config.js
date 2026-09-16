2c2dcafc20cb2dfee17027d3271f5914092d849f
WIN11大南邦基\hsieh <teacher.hsieh@gmail.com>
2026-09-16 10:23:16 +0800

    Configure Firebase project for v3

--- v3/firebase-config.js
+++ v3/firebase-config.js
@@ -1,1 +1,16 @@
-export { firebaseConfig, classroomSettings } from "./firebase-config.example.js";
+export const firebaseConfig = {
+  apiKey: "AIzaSyDYAjUZU4UDoSqbfPye52CvKX1NPlfd-Jo",
+  authDomain: "x9-factor-game.firebaseapp.com",
+  projectId: "x9-factor-game",
+  storageBucket: "x9-factor-game.firebasestorage.app",
+  messagingSenderId: "553664934423",
+  appId: "1:553664934423:web:a961ae02c12db091606252",
+  measurementId: "G-1KV1TG9MNV"
+};
+
+export const classroomSettings = {
+  maxStudents: 100,
+  adminEmails: [
+    "teacher.hsieh@gmail.com"
+  ]
+};
